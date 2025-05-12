62004d4525ebf3d707fc02c4a966792691c510e4
Varad J <varad.gcs@gmail.com>
2025-05-12 11:28:55 +0530

    Update firebase-forgot-password.js

--- firebase-forgot-password.js
+++ firebase-forgot-password.js
@@ -1,34 +1,37 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+import {
+  getAuth,
+  sendPasswordResetEmail
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN9i_dRUtuHim0RQO8P1Xx_0SsPAUMjb0",
-  authDomain: "spectracell-provider-auth.firebaseapp.com",
-  projectId: "spectracell-provider-auth",
-  storageBucket: "spectracell-provider-auth.firebasestorage.app",
-  messagingSenderId: "893082488254",
-  appId: "1:893082488254:web:850b33c83cc82d42797def",
-  measurementId: "G-59N0KFCC25"
+  apiKey: "AIzaSyA9jyDnF9U111CsOgt8wiayoXYdSgFym44",
+  authDomain: "spectracell-test.firebaseapp.com",
+  projectId: "spectracell-test",
+  storageBucket: "spectracell-test.firebasestorage.app",
+  messagingSenderId: "867376517322",
+  appId: "1:867376517322:web:33d56ac7d0888fcb80a433"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+// Modular initialization:
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 async function forgotPassword() {
   const email = document.getElementById("user-email").value;
-
   if (!email) {
     alert("Please enter your email address.");
     return;
   }
-
   try {
-    await auth.sendPasswordResetEmail(email);
+    await sendPasswordResetEmail(auth, email);
     alert("A password reset email has been sent to " + email);
   } catch (error) {
     alert(`Error: ${error.message}`);
   }
 }
 
-document.getElementById("forgot-password-button").addEventListener("click", forgotPassword);
+document
+  .getElementById("forgot-password-button")
+  .addEventListener("click", forgotPassword);
+
